b9985d307301e7fd5274bb9c7e7fb482e855d047
d.savuljesku <savuljesku@hallowelt.com>
2021-09-23 14:35:32 +0200

    Improve export process
    - Run multiple smaller API requests to gather data
    - Show loading notice to user while its doing it
    
    This is just a slight improvement. It wont fail anymore, but will still take long.
    
    This was done for UserManager table, but some other tables are also included.
    
    [3.2.x]
    
    ERM 25831
    
    Change-Id: I65d9c029d90e0bb0765d15c7580cf851610b343b

--- resources/bluespice.exportTables.CRUDGridPanel.button.js
+++ resources/bluespice.exportTables.CRUDGridPanel.button.js
@@ -1,95 +1,133 @@
 ( function( mw, $, d ) {
-BsExportTablesHTMLTableProvider = {
-	_makeHTMLTableProvider: function( sender ) {
-	return {
-		getHTMLTable: function() {
-			var dfd = $.Deferred();
-			var lastRequest = sender.strMain.getProxy().getLastRequest();
-			var params = lastRequest.getParams();
+	var dataProvider = function( sender, batchSize ) {
+		this.batchSize = batchSize || 200;
+		this.sender = sender;
+	};
 
-			//This is ugly... unfortunately most AJAX interfaces can not
-			//handle requests without those parameters
-			params.page = 1;
-			params.limit = 9999999;
-			params.start = 0;
+	dataProvider.prototype.getData = function () {
+		var dfd = $.Deferred();
 
-			var url = lastRequest.getUrl(); //TODO: Modify _dc flag!
+		this.dataColumns = [];
+		this.queryParams = this.sender.strMain.getProxy().getLastRequest().getParams();
+		this.queryParams.start = 0;
+		this.queryParams.limit = this.batchSize;
 
-			Ext.Ajax.request({
-				url:url,
-				params: params,
-				success: function( response ){
-					var resp = Ext.decode( response.responseText );
-					var proxy = sender.strMain.getProxy();
-					var reader = proxy.getReader();
-					var rows = resp[reader._rootProperty];
-					var columns = sender.grdMain.columns;
-					var row = null;
-					var col = null;
-					var value = '';
-					var $table = $('<table>');
-					var $row = null;
-					var $cell = null;
-					var record = null;
+		var columns = this.sender.grdMain.columns;
+		for( var i = 0; i < columns.length; i++ ) {
+			col = columns[i];
+			if( col instanceof Ext.grid.ActionColumn )
+				continue;
 
-					$row = $('<tr>');
-					$table.append($row);
-					for( var i = 0; i < columns.length; i++ ) {
-						col = columns[i];
-						if( col instanceof Ext.grid.ActionColumn )
-							continue;
+			if( col.hidden === true )
+				continue;
 
-						if( col.hidden === true )
-							continue;
+			this.dataColumns.push( col );
+		}
 
-						$cell = $('<td>');
-						$row.append( $cell );
-						$cell.append( col.header || col.text );
-					}
 
-					for( var i = 0; i < rows.length; i++ ) {
-						row = rows[i];
-						$row = $('<tr>');
-						record = new sender.strMain.model( row );
-						$table.append($row);
+		var url = this.sender.strMain.getProxy().getLastRequest().getUrl(), //TODO: Modify _dc flag!
+			data = [];
+		this.query( data, dfd, url, 0 );
 
-						for( var j = 0; j < columns.length; j++ ) {
-							col = columns[j];
-							if( col instanceof Ext.grid.ActionColumn )
-								continue;
+		return dfd.promise();
+	};
 
-							if( col.hidden === true )
-								continue;
-
-							$cell = $('<td>');
-							$row.append( $cell );
-
-							if( col.renderer && Ext.isFunction( col.renderer ) ) {
-								value = col.renderer(
-									row[col.dataIndex],
-									{}, //Cell meta... we don't have any
-									record,
-									i,
-									j,
-									sender.strMain,
-									sender.grdMain.getView()
-								);
-							}
-							else {
-								value = row[col.dataIndex];
-							}
-							$cell.append( value );
-						}
-					}
-
-					dfd.resolve( '<table>' + $table.html() + '</table>' );
+	dataProvider.prototype.query = function ( data, dfd, url, start ) {
+		Ext.Ajax.request( {
+			url: url,
+			params: $.extend( {}, this.queryParams, { start: start } ),
+			success: function( response ) {
+				if ( !response.hasOwnProperty( 'responseText' ) ) {
+					dfd.reject();
+					return;
 				}
-			});
-			return dfd;
+				response = JSON.parse( response.responseText );
+				if ( response.results.length === 0 ) {
+					dfd.resolve( data );
+					return;
+				}
+
+				data = data.concat( response.results );
+				this.query( data, dfd, url, start + this.batchSize + 1 );
+			}.bind( this ),
+			fail: function() {
+				dfd.reject();
+			}
+		} );
+	};
+
+	dataProvider.prototype.getHTMLTable = function () {
+		this.setLoading( true );
+		var dfd = $.Deferred();
+		this.getData().done( function( data ) {
+			this.generateTable( data ).done( function( table ) {
+				this.setLoading( false );
+				dfd.resolve( table );
+			}.bind( this ) );
+		}.bind( this ) ).fail( function() {
+			this.setLoading( false );
+			dfd.reject();
+		}.bind( this ) );
+
+		return dfd.promise();
+	};
+
+	dataProvider.prototype.setLoading = function ( loading ) {
+		if ( loading ) {
+			if ( !this.loadingWin ) {
+				this.loadingWin = Ext.create( 'BS.ExportTables.dialog.Loading' );
+				this.loadingWin.show();
+			}
+		} else {
+			this.loadingWin.destroy();
 		}
 	};
-}
-};
+
+	dataProvider.prototype.generateTable = function ( data ) {
+		var dfd = $.Deferred(),
+			$table = $('<table>'), $row = null, $cell = null,
+			record = null, column, i, value;
+
+		$row = $('<tr>');
+		$table.append($row);
+		for( i = 0; i < this.dataColumns.length; i++ ) {
+			column = this.dataColumns[i];
+			$cell = $('<td>');
+			$row.append( $cell );
+			$cell.append( column.header || column.text );
+		}
+
+		for( i = 0; i < data.length; i++ ) {
+			var row = data[i];
+			$row = $('<tr>');
+			record = new this.sender.strMain.model( row );
+			$table.append($row);
+			for( j = 0; j < this.dataColumns.length; j++ ) {
+				column = this.dataColumns[j];
+				$cell = $('<td>');
+				$row.append( $cell );
+				if( column.renderer && Ext.isFunction( column.renderer ) ) {
+					value = column.renderer(
+						row[column.dataIndex],
+						{}, //Cell meta... we don't have any
+						record,
+						i,
+						j,
+						this.sender.strMain,
+						this.sender.grdMain.getView()
+					);
+				}
+				else {
+					value = row[column.dataIndex];
+				}
+				$cell.append( value );
+			}
+		}
+
+		dfd.resolve( '<table>' + $table.html() + '</table>' );
+
+		return dfd.promise();
+	};
 
 $( d ).on('BSCRUDPanelInitComponent', function( e, sender ) {
 	if( typeof(BS.CRUDGridPanel) === "undefined" || sender instanceof BS.CRUDGridPanel === false ) {
@@ -98,8 +136,7 @@ $( d ).on('BSCRUDPanelInitComponent', function( e, sender ) {
 
 	var provider = sender.getHTMLTable
 		? sender
-		: BsExportTablesHTMLTableProvider._makeHTMLTableProvider( sender )
-	;
+		: new dataProvider( sender );
 	if( sender.tbar ) {
 		sender.tbar.add( '->' );
 		sender.tbar.add( {
@@ -118,7 +155,6 @@ $( d ).on('BSCRUDPanelInitComponent', function( e, sender ) {
 				htmlTableProvider: provider
 			})
 		} );
-		return;
 	}
 });
 
@@ -126,7 +162,9 @@ $( d ).on('BSPanelInitComponent', function( e, sender ) {
 	if( typeof sender === 'undefined' || !sender.getHTMLTable ) {
 		return;
 	}
-	var provider = sender;
+	var provider = sender.getHTMLTable
+		? sender
+		: new dataProvider( sender );
 
 	if( sender.tbar ) {
 		sender.tbar.add( '->' );
@@ -146,15 +184,13 @@ $( d ).on('BSPanelInitComponent', function( e, sender ) {
 				htmlTableProvider: provider
 			})
 		} );
-		return;
 	}
 });
 
 $( d ).on('BSPermissionManagerAfterInitComponent', function( e, sender ) {
 	var provider = sender.getHTMLTable
 		? sender
-		: BsExportTablesHTMLTableProvider._makeHTMLTableProvider( sender )
-	;
+		: new dataProvider( sender );
 
 	sender.tbar.push( {
 		text: mw.message('bs-exporttables-menu').plain(),
