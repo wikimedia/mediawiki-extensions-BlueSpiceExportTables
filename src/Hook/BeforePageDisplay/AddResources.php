7c9cf0fa6b78ba1c75b2717d2a58c09425fa1df6
hslater <hamish.slater@hallowelt.com>
2024-04-04 21:47:53 +1300

    Fix missing Export table
    Merging these RL modules breaks it
    
    ERM34750
    
    [REL1_39, REL1_39-4.4.x, master]
    
    Change-Id: I714562a4769e853736c26f440d7028c5174cef94

--- src/Hook/BeforePageDisplay/AddResources.php
+++ src/Hook/BeforePageDisplay/AddResources.php
@@ -6,7 +6,10 @@ class AddResources extends \BlueSpice\Hook\BeforePageDisplay {
 
 	protected function doProcess() {
 		$this->out->addModuleStyles( [ 'ext.bluespice.exportTables.contentTables.styles' ] );
-		$this->out->addModules( [ 'ext.bluespice.exportTables.main' ] );
+		$this->out->addModules( [
+			'ext.bluespice.exportTables.main',
+			'ext.bluespice.exportTables.CRUDGridPanel.button'
+		] );
 
 		$this->out->addJsConfigVars(
 			'bsgExportTablesMenuTargetSelector',
